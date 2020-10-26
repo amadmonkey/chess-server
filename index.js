d9345a43fbd4ef56f7f661bfd6d727e9dbbcd585
Arcenio <aquinoarcie@gmail.com>
2020-10-26 21:26:12 +0800

    generateId fix

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ class LOBBY {
     }
     generateId({length = 4}){
         // https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-        let result, characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', charactersLength = characters.length;
+        let result = '', characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', charactersLength = characters.length;
         for (let i = 0; i < length; i++ ) {
             result += characters.charAt(Math.floor(Math.random() * charactersLength));
         }
